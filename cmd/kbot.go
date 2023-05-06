f48b6cb5de6ba0f234faa5fb53abe8ce5507df18
Searge <ashikcygan@gmail.com>
2023-05-06 23:50:07 +0300

    :sparkles: (bot): Create the kbot
    Write down plain logic ane error handling. Connect app to the Telegram

---
+++ cmd/kbot.go
@@ -0,0 +1,72 @@
+/*
+Copyright © 2023 @Searge
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+var (
+	// TeleToken bot
+	TeleToken string = os.Getenv("TELE_TOKEN")
+)
+
+// kbotCmd represents the kbot command
+var kbotCmd = &cobra.Command{
+	Use:     "kbot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("kbot %s started\n", appVersion)
+		kbot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("Please check your TELE_TOKEN env variable, %s", err)
+		}
+
+		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Printf(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+
+			switch payload {
+				case "hello":
+					err = m.Send((fmt.Sprintf("Hello! I'm kbot %s", appVersion)))
+
+			}
+
+			return err
+		})
+		kbot.Start()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(kbotCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// kbotCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// kbotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
